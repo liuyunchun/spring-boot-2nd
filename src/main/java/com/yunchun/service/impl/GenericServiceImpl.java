b44bb0ff7a52a6988e1342c0ec18b3f3569e308b
sungyeh <sungyeh@tist.com.tw>
2020-05-19 17:39:00 +0800

    調整注入方式

--- src/main/java/com/yunchun/service/impl/GenericServiceImpl.java
+++ src/main/java/com/yunchun/service/impl/GenericServiceImpl.java
@@ -1,15 +1,20 @@
 package com.yunchun.service.impl;
 
 import com.yunchun.service.GenericService;
-import lombok.Setter;
+import lombok.Getter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 public abstract class GenericServiceImpl<T, R extends JpaRepository<T, String>> implements GenericService<T, R> {
-    @Setter
+    @Getter
     R repository;
 
+    @Resource
+    public void setRepository(R repository) {
+        this.repository = repository;
+    }
 
     @Override
     public T insert(T t) {
--- src/main/java/com/yunchun/service/impl/SysCodeServiceImpl.java
+++ src/main/java/com/yunchun/service/impl/SysCodeServiceImpl.java
@@ -5,23 +5,20 @@ import com.yunchun.repository.SysCodeRepository;
 import com.yunchun.service.SysCodeService;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
 @Service("com.yunchun.service.impl.SysCodeServiceImpl")
 public class SysCodeServiceImpl extends GenericServiceImpl<SysCode, SysCodeRepository> implements SysCodeService {
 
-    @Resource
-    private SysCodeRepository sysCodeRepository;
-
-    @PostConstruct
-    public void init() {
-        setRepository(sysCodeRepository);
-    }
+//    @Resource
+//    private SysCodeRepository sysCodeRepository;
+//
+//    @PostConstruct
+//    public void init() {
+//        setRepository(sysCodeRepository);
+//    }
 
     @Override
     public SysCode find(String id) {
-        return sysCodeRepository.findOneById(id);
+        return getRepository().findOneById(id);
     }
 
 }
